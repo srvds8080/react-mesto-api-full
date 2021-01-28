e04faa26517a3411ebdfb8b585d31f2644f15984
srvds8080 <srvds@yandex.ru>
2021-01-28 14:29:21 +0300

    fix: Изменить имя переменной пути базы данных

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,11 +1,11 @@
 const OK_CODE = 200;
 const CREATE_CODE = 201;
 const INTERNAL_SERVER_ERROR_CODE = 500;
-const urlBD = 'mongodb://localhost:27017/mestodb';
+const pathDataBase = 'mongodb://localhost:27017/mestodb';
 
 module.exports = {
   OK_CODE,
   CREATE_CODE,
   INTERNAL_SERVER_ERROR_CODE,
-  urlBD,
+  pathDataBase,
 };
