62da3ddc77fa29f2541b974f77af3c70794e45ed
srvds8080 <srvds@yandex.ru>
2021-01-31 17:52:19 +0300

    bagfix: Исправить синтаксическую ошибку

--- backend/models/card.js
+++ backend/models/card.js
@@ -18,7 +18,7 @@ const cardSchema = new mongoose.Schema({
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'uesr',
+    ref: 'user',
   },
   likes: [{
     type: mongoose.Schema.Types.ObjectId,
