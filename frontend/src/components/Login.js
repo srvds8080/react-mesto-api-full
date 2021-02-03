25b7972fa21d8611465aea71dcea84ee180e0a56
srvds8080 <srvds@yandex.ru>
2021-02-03 22:47:31 +0300

    feat: Сделать валидацтъию формы логина и регистрации

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,17 +1,29 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const Login = (props) => {
   const { onSubmit } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+  const [isFormValid, setIsFormValid] = useState(false);
 
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
+  const handleValidate = useCallback((e) => {
+    setErrors((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.validity.valid ? '' : e.target.validationMessage,
+    }));
+  }, [errors]);
 
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    if (e.target.name === 'email') {
+      setEmail(value);
+      handleValidate(e);
+    } else {
+      setPassword(value);
+      handleValidate(e);
+    }
   };
   const handleSubmit = useCallback((e) => {
     e.preventDefault();
@@ -20,6 +32,13 @@ const Login = (props) => {
       password,
     });
   }, [email, password]);
+    // check form on errors
+  useEffect(() => {
+    if (!Object.values(errors).every((key) => key === '')) {
+      return setIsFormValid(false);
+    }
+    return setIsFormValid(true);
+  }, [handleOnChange]);
   return (
     <section className="login">
       <h3 className="login__title">Вход</h3>
@@ -30,25 +49,40 @@ const Login = (props) => {
       >
         <input
           className="login__input"
+          name="email"
           type="email"
           placeholder="Email"
           required
-          onChange={handleEmail}
-          value={email}
+          onChange={handleOnChange}
+          value={email || ''}
         />
+        <span
+          className="popup__form-error"
+          id="popup-input_type_description-error"
+        >
+          {errors.email}
+        </span>
         <input
           className="login__input"
+          name="password"
           type="password"
           placeholder="Пароль"
           required
-          onChange={handlePassword}
-          value={password}
+          onChange={handleOnChange}
+          value={password || ''}
         />
+        <span
+          className="popup__form-error"
+          id="popup-input_type_description-error"
+        >
+          {errors.password}
+        </span>
         <button
           className="login__submit-button"
           type="submit"
           aria-label="Войти"
           value="Войти"
+          disabled={!isFormValid}
         >
           Войти
         </button>
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -6,13 +6,25 @@ const Register = (props) => {
   const { onSubmit } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+  const [isFormValid, setIsFormValid] = useState(false);
 
-  const handleEmail = (e) => {
-    setEmail(e.target.value);
-  };
+  const handleValidate = useCallback((e) => {
+    setErrors((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.validity.valid ? '' : e.target.validationMessage,
+    }));
+  }, [errors]);
 
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    if (e.target.name === 'email') {
+      setEmail(value);
+      handleValidate(e);
+    } else {
+      setPassword(value);
+      handleValidate(e);
+    }
   };
   const handleSubmit = useCallback((e) => {
     e.preventDefault();
@@ -21,6 +33,13 @@ const Register = (props) => {
       password,
     });
   }, [email, password]);
+    // check form on errors
+  useEffect(() => {
+    if (!Object.values(errors).every((key) => key === '')) {
+      return setIsFormValid(false);
+    }
+    return setIsFormValid(true);
+  }, [handleOnChange]);
   return (
     <section className="login">
       <h3 className="login__title">Регистрация</h3>
@@ -31,25 +50,40 @@ const Register = (props) => {
       >
         <input
           className="login__input"
+          name="email"
           type="email"
           placeholder="Email"
           required
-          onChange={handleEmail}
-          value={email}
+          onChange={handleOnChange}
+          value={email || ''}
         />
+        <span
+          className="popup__form-error"
+          id="popup-input_type_description-error"
+        >
+          {errors.email}
+        </span>
         <input
           className="login__input"
+          name="password"
           type="password"
           placeholder="Пароль"
           required
-          onChange={handlePassword}
-          value={password}
+          onChange={handleOnChange}
+          value={password || ''}
         />
+        <span
+          className="popup__form-error"
+          id="popup-input_type_description-error"
+        >
+          {errors.password}
+        </span>
         <button
           className="login__submit-button"
           type="submit"
           aria-label="Войти"
           value="Войти"
+          disabled={!isFormValid}
         >
           Зарегистрироваться
         </button>
